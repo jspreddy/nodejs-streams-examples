da3cd5a1131b38b1b909fb72eb137dbc007930e2
sai-ubuntu <jspreddy@yahoo.com>
2019-05-20 02:31:15 -0400

    server with read file example

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import _ from 'lodash';
 import fs from 'fs';
 import express from 'express';
+import url from 'url';
 
 
 const app = express();
 const port = 65345;
 
+const smallFile = './assets/test.txt';
 
-const fileServHandler = (request, response) => {
-  // download file from location
-  // file url: https://s3.amazonaws.com/jspreddy-gov-data/nyc-taxi-tlc/yellow_tripdata_2017-01.csv
+// $> wget https://norvig.com/big.txt
+const mediumFile = './assets/big.txt';
 
-  // send file to user.
+// $> wget https://s3.amazonaws.com/jspreddy-gov-data/nyc-taxi-tlc/yellow_tripdata_2017-01.csv
+const bigFile = './assets/yellow_tripdata_2017-01.csv';
+
+const file = smallFile;
+
+const localSmallFileHandler = (request, response) => {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      const content = data.toString() + _.repeat('-', 20);
+      console.log(content);
+      response.send(content);
+    }
+  });
 };
 
-app.get('/file', fileServHandler);
-
+app.get('/local-small-file', localSmallFileHandler);
 
 app.listen(
   port,
