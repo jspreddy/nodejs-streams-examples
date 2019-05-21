d2a71e75be481e47a28c62926695d60fb05e5eeb
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2019-05-21 15:30:18 -0400

    third example

---
+++ src/03_third.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+
+const file = './destination/created_from_stdin.txt';
+
+const stream = fs.createWriteStream(file);
+// NOTE: this will overwrite the file everytime we execute this file.
+
+// const stream = fs.createWriteStream(file, {
+//   flags: 'a',
+//   encoding: null,
+//   mode: '0666',
+// });
+
+process.stdin.pipe(stream);
+
+// TIP: to see the text coming in realtime,
+// you can run a command in a separate terminal to tail the file
+// $> tail -f ./destination/created_from_stdin.txt
