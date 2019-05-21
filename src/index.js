5678d551a825c712e3c86e6f4cfd6cc003d87c1a
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2019-05-21 00:11:37 -0400

    lets do it without express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import _ from 'lodash';
 import fs from 'fs';
-import express from 'express';
-import url from 'url';
-
-
-const app = express();
-const port = 65345;
 
 const smallFile = './assets/test.txt';
 
@@ -17,21 +11,11 @@ const bigFile = './assets/yellow_tripdata_2017-01.csv';
 
 const file = smallFile;
 
-const localSmallFileHandler = (request, response) => {
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      const content = data.toString() + _.repeat('-', 20);
-      console.log(content);
-      response.send(content);
-    }
-  });
-};
-
-app.get('/local-small-file', localSmallFileHandler);
-
-app.listen(
-  port,
-  () => console.log(`App listening on port ${port}`),
-);
+fs.readFile(file, (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    const content = data.toString() + _.repeat('-', 20);
+    console.log(content);
+  }
+});
